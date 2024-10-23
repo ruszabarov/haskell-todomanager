dddf58e2ccba9f68669831ebc87beaff6a6e52c0
Your name <ruszabarov@gmail.com>
2024-10-23 11:41:15 -0400

    initial commit

---
+++ main.hs
@@ -0,0 +1,46 @@
+import Data.List (isInfixOf)
+import System.IO (hFlush, stdout)
+
+main = do
+  putStrLn "Welcome to Task Manager 3000"
+  taskManager []
+
+menu = do
+  putStrLn "\nBelow are the options:"
+  putStrLn "\tadd"
+  putStrLn "\tprint"
+  putStrLn "\tsearch"
+
+printTasks (x : xs) = do
+  putStrLn $ "\t" ++ x
+  printTasks xs
+printTasks [] = do
+  putStr ""
+
+searchTasks [] term = putStrLn $ "\tCould not find " ++ term
+searchTasks (x : xs) term = do
+  if x == term
+    then putStrLn $ "\tFound: " ++ x
+    else searchTasks xs term
+
+taskManager tasks = do
+  menu
+  putStrLn "Enter option:"
+  choice <- getLine
+  case choice of
+    "add" -> do
+      putStrLn "Enter Task to Add:"
+      newTask <- getLine
+      let updatedTasks = tasks ++ [newTask]
+      taskManager updatedTasks
+    "print" -> do
+      putStrLn "Here are your tasks:"
+      printTasks tasks
+      taskManager tasks
+    "search" -> do
+      putStrLn "Enter Task to Search:"
+      searchTerm <- getLine
+      searchTasks tasks searchTerm
+      taskManager tasks
+    _ -> do
+      putStrLn "Error"
